57ca7455a4d282427678bee1f1ea71fca3c19274
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-09-09 18:42:46 +0100

    Ajuste na fotografia

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -2,15 +2,14 @@
 @include('inclusao.header')
 <title>Início</title>
 
-
-<main>
+<main class="p-4">
     <div class="container g-3 border " style="min-height: inherit">
         <div class="p-4 ">
-            <div class="col-2 border mb-2" style="min-height: 100px; ">
-            <a href="{{ asset('assets/img/logotipo.jpg') }}">
-                <img style="max-height: inherit; max-width: 175px; object-fit: cover" 
-                src="{{ asset('assets/img/logotipo.jpg') }}" class="img-fluid" alt="Não encontrado">
-            </a>   
+            <div class="col-2 border mb-2" style="max-height: 150px; overflow: hidden;">
+                <a href="{{ asset('assets/img/logo.jpg') }}">
+                    <img style="max-height: inherit; max-width: 175px; object-fit: cover;"
+                        src="{{ asset('assets/img/logo.jpg') }}" class="img-fluid" alt="Não encontrado">
+                </a>
             </div>
 
             <div class="container col-12 border mb-2">
@@ -21,12 +20,17 @@
             </div>
 
             <div class="container col-12 border mb-2 pt-2">
-                <h4><b>Saldo da conta: 1 000 000 00,kz</b></h4>
+                <h4><i class="fas fa-cash pe-2"></i> <b>Saldo da conta: 1 000 000 00,kz</b></h4>
+            </div>
+
+            <div>
+                <button type="submit">
+                    Terminar Sessão
+                </button>
             </div>
         </div>
     </div>
 </main>
-
 
 @include('inclusao.footer')
 @include('inclusao.foot')
