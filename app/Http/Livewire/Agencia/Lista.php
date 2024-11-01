457232e6012a6e589d638e2db2698f8d25b509aa
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-11-01 12:04:38 +0100

    Listando agencias

--- app/Http/Livewire/Agencia/Lista.php
+++ app/Http/Livewire/Agencia/Lista.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire\Agencia;
 
+use App\Models\Agencia;
+use App\Models\Morada;
 use Livewire\Component;
 
 class Lista extends Component
 {
+    public $agencias;
+
     public function render()
     {
+        $this->agencias = Agencia::all();
         return view('livewire.agencia.lista');
+    }
+
+    public function buscarLocalizacao($id){
+        return Morada::find($id);
+    }
+
+    public function eliminarAgencia($id){
+        $agencia = Agencia::find($id);
+        $agencia->delete();
+        $this->emit('alerta', ['mensagem' => 'Agencia eliminada com sucesso', 'icon' => 'success', 'tempo' => 3000]);
     }
 }
--- resources/views/livewire/agencia/lista.blade.php
+++ resources/views/livewire/agencia/lista.blade.php
@@ -1,3 +1,69 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+@php
+    use App\Models\DadosPessoais;
+    use App\Models\Acesso;
+    $usuario = Auth::user();
+    $dados = DadosPessoais::where('id_usuario', $usuario->id)->first();
+    $acesso = Acesso::find($usuario->id_acesso);
+@endphp
+
+<div class="container g-3 border " style="min-height: inherit">
+    <div class="p-4 ">
+        @include('inclusao.tag_usuario')
+
+        <div class="container col-12 border mb-2">
+            <h1 class="text-center text-md-start pt-3 pb-4">Lista de agencias</h1>
+
+            <div class="col-12 ">
+                <div class="table-responsive">
+                    <table id="minhaTabela" class="table datatablePT table-hover pt-3">
+                        <thead class="">
+                            <tr>
+                                <th class="bg-primary text-white" style="white-space: nowrap">
+                                    Id
+                                </th>
+
+                                <th class="bg-primary text-white" style="white-space: nowrap">
+                                    Nº de Identificação
+                                </th>
+
+                                <th class="bg-primary text-white" style="white-space: nowrap">
+                                    Localizacao
+                                </th>
+
+                                <th class="bg-primary text-white" style="white-space: nowrap">
+                                    Acção
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody class="text-white">
+                            @foreach ($agencias as $agencia)
+                                @php
+                                    $localizacao = $this->buscarLocalizacao($agencia->localizacao);
+                                @endphp
+                                <tr class="text-white">
+                                    <td>{{ $agencia->id }}</td>
+                                    <td>{{ $agencia->num_indent }}</td>
+                                    <td>{{ $localizacao->provincia }} : {{ $localizacao->endereco }} </td>
+                                    <td class="text-center">
+                                        <a href="{{route("agencia.actualizar", $agencia->id)}}">
+                                            <button class="bg-info" type="button" style="width: 40px">
+                                                <i class="fas fa-pen"></i>
+                                            </button>
+                                        </a>
+
+                                        <button class="bg-danger" type="button"
+                                            wire:click="eliminarAgencia({{ $agencia->id }})" style="width: 40px">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
