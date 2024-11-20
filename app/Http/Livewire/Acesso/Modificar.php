7e91eae4909590aa3ee1baf7a7a033dc7e89f5b5
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-11-20 14:06:54 +0100

    Mudanças de acesso comprovativo

--- app/Http/Livewire/Acesso/Modificar.php
+++ app/Http/Livewire/Acesso/Modificar.php
@@ -4,7 +4,9 @@ namespace App\Http\Livewire\Acesso;
 
 use App\Models\Acesso;
 use App\Models\DadosPessoais;
+use App\Models\Historico;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Livewire\Component;
 
@@ -27,6 +29,12 @@ class Modificar extends Component
 
             User::where("id", $id_usuario)->update(["id_acesso" => $this->usarioIdAcesso]);
             $this->emit('alerta', ['mensagem' => $nomeCompleto . ' adicionado como ' . $acesso->tipo, 'icon' => 'success', 'tempo'=>3000]);
+            $dadosPessoais = DadosPessoais::where("id_usuario", $id_usuario)->first();
+            Historico::create([
+                "id_usuario" => Auth::user()->id,
+                "tema" => "Modificação de Acesso de Conta",
+                "descricao" => "Modificou o acesso de conta do/a {$dadosPessoais->nome} {$dadosPessoais->sobrenome} para {$acesso->tipo}",
+            ]);
             return redirect()->route('acesso.modificar');
         }else{
             $this->emit('alerta', ['mensagem' => 'O acesso deve ser diferente do encontrado, escolha outro tipo de acesso', 'icon' => 'warning', 'tempo'=>4000]);
